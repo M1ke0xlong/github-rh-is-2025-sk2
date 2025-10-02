d2e6967c29dbbb51a3a623ab003008a7be59b3d9
Roman Hozak <roman.hozak.021@g.pslib.cz>
2025-10-02 14:07:07 +0200

    #3 Vypis řady - vstup první hodnoty

---
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -0,0 +1,41 @@
+﻿//print v konzoli
+Console.WriteLine("************************");
+Console.WriteLine("***Výpis číselné řady***");
+Console.WriteLine("*******Roman Hozák******");
+Console.WriteLine("********2.10.2025*******");
+Console.WriteLine("************************");
+Console.WriteLine("************************");
+
+//definování proměnných
+int first, last, step;
+//lepsi vstup
+Console.WriteLine("Zadejte první číslo řady (celé číslo):");
+while (!int.TryParse(Console.ReadLine(), out first))
+{
+    Console.WriteLine("Špatně zadaný vstup, zkuste to znovu a jako celé číslo");
+}
+/*
+//první číslo
+Console.WriteLine("first = ");
+first = int.Parse(Console.ReadLine());
+//poslední číslo
+Console.WriteLine("last = ");
+last = int.Parse(Console.ReadLine());
+//kroky
+Console.WriteLine("step = ");
+step = int.Parse(Console.ReadLine());
+//vypsání prvního čísla a loop
+Console.WriteLine("\n" + first);
+while (true)
+{
+    if ((first + step) <= last)
+    {
+        first = first + step;
+        Console.WriteLine(first);
+    }
+    else
+    {
+        break;
+    }
+}
+*/
