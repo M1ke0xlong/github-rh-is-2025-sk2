5bcf3ae9cb84ec76859b4f8e71507b60c5de66d9
Roman Hozak <roman.hozak.021@g.pslib.cz>
2025-11-13 13:58:11 +0100

    #9 maximum, minimum a další informace o vygenerovaných číslech

--- IS-Programy/program001-vypis-rady/obj/Debug/net9.0/program001-vypis-rady.AssemblyInfo.cs
+++ IS-Programy/program001-vypis-rady/obj/Debug/net9.0/program001-vypis-rady.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e018e8cd7214092acf0ace52c54c846a960bc6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d87a78dedcb99e5d817d44bbef3b16b9aaa5c302")]
 [assembly: System.Reflection.AssemblyProductAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program001-vypis-rady")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
